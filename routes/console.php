54b3640a2564e25aef391c9c3c09e004a3c2b37e
“[Hendry <hendryputra934@gmail.com>
2025-05-15 15:00:25 +0700

    add automatic publish on scheduled post

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('posts:publish-scheduled-posts', function () {
+    $updated = Post::where('status', Post::TYPE_SCHEDULED)
+        ->where('published_at', '<=', now())
+        ->update(['status' => Post::TYPE_ACTIVE]);
+
+    $this->info("{$updated} post(s) have been activated.");
+})->purpose('Publish scheduled posts')->everyMinute();
