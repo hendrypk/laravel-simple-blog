de82bc91ac15a76219e39b990a6c5b11831bfbcf
“[Hendry <hendryputra934@gmail.com>
2025-05-15 14:50:46 +0700

    modify home controlle

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,14 @@ use function view;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index () {
-        $posts = Post::where('user_id', Auth::id())->get();
-        return view ('home', compact('posts'));
+    public function index ():View
+    {
+        return view('home', [
+            'posts' => Post::where('user_id', Auth::id())->get()
+        ]);
     }
 }
